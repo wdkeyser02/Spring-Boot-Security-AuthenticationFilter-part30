5038344854b9e7904635510fd7dcf67c810696fa
Willy De Keyser <spring.boot.programming@gmail.com>
2026-06-22 11:25:37 +0200

    Update

--- Spring-Boot-Security-AuthenticationFilter-part30/src/main/java/willydekeyser/SpringBootSecurityAuthenticationFilterPart30Application.java
+++ Spring-Boot-Security-AuthenticationFilter-part30/src/main/java/willydekeyser/SpringBootSecurityAuthenticationFilterPart30Application.java
@@ -2,8 +2,10 @@ package willydekeyser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug=true)
 public class SpringBootSecurityAuthenticationFilterPart30Application {
 
 	public static void main(String[] args) {
